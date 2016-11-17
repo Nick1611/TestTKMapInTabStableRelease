c8ca069e59ccf9a7dca48ed23a9ee581f6c63619
Nick1611 <nick.myappsgallery@gmail.com>
2016-11-18 00:29:37 +0800

    Test With TK Map Stable Release

---
+++ TestTKMapInTab/TestTKMapInTab/TestPage1.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace TestTKMapInTab
+{
+    public partial class TestPage1 : ContentPage
+    {
+        public TestPage1()
+        {
+            InitializeComponent();
+        }
+    }
+}
